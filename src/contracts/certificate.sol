7926dc4bcab3c41afff7bbcb205b87f45770cbf5
blackchapel <kc.dark@protonmail.com>
2023-02-17 15:57:28 +0530

    smart contract

---
+++ src/contracts/certificate.sol
@@ -0,0 +1,63 @@
+pragma solidity >=0.5.0;
+
+contract certisecure {
+
+    uint public uCount;
+    uint public cCount;
+    mapping(uint => User) public users;
+    mapping(uint => Certificate) public certificates;
+
+    struct User {
+        uint id;
+        address userAddress;
+        string name;
+        string userType;
+    }
+
+    struct Certificate {
+        uint id;
+        string uid;
+        string url;
+        address issuer;
+        address recipient;
+        bool isValid;
+        string desc;
+    }
+
+    event generatedCertificate(string uid);
+
+    function createUser(string memory _name, string memory _type) external {
+        uCount++;
+        users[uCount] = User(uCount, msg.sender, _name, _type);
+    }
+
+
+    function issueCertificate(string memory _url, address _recipient, string memory _desc) external {
+        require(bytes(_url).length > 0);
+        require(bytes(_desc).length > 0);
+
+        cCount++;
+        string memory uid = "s27ghdb2648nd";
+        certificates[cCount] = Certificate(cCount, uid, _url, msg.sender, _recipient, true, _desc);
+        emit generatedCertificate(uid);
+    }
+
+    function revokeCertificate(uint _id) external {
+        Certificate memory cert = certificates[_id];
+        require(cert.issuer == msg.sender);
+        cert.isValid = false;
+        certificates[_id] = cert;        
+    }
+
+    function verifyCertificate(string memory _uid) external view returns(bool, uint) {
+        bool valid = false;
+        uint id = 0;
+        for(uint i = 1; i <= cCount; ++i) {
+            if(keccak256(abi.encodePacked(certificates[i].uid)) == keccak256(abi.encodePacked(_uid))) {
+                valid = certificates[i].isValid;
+                id = i;
+            } 
+        }
+        return (valid, id);
+    }
+}
---
+++ truffle-config.js
@@ -0,0 +1,34 @@
+const ContractKit = require('@celo/contractkit');
+const Web3 = require('web3');
+require('dotenv').config();
+
+const web3 = new Web3('https://alfajores-forno.celo-testnet.org');
+const kit = ContractKit.newKitFromWeb3(web3);
+
+kit.connection.addAccount(process.env.CELO_PRIVATE_KEY);
+
+module.exports = {
+    networks: {
+        development: {
+            host: '127.0.0.1',
+            port: 8545,
+            network_id: '*'
+        },
+        alfajores: {
+            provider: kit.connection.web3.currentProvider,
+            network_id: 44787
+        }
+    },
+
+    contracts_directory: './src/contracts/',
+    contracts_build_directory: './src/abis/',
+    compilers: {
+        solc: {
+            version: '0.7.1',
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    }
+};
